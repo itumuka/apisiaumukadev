f45a1006d1585de2df7c8cd7552707c175baeff8
LittleDitto <ditoanggitac@gmail.com>
2026-05-21 13:54:56 +0700

    Add Table Migrations

---
+++ database/migrations/skripsi/2026_05_21_000003_create_akd_skripsi_sempro_mk_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAkdSkripsiSemproMkTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('akd_skripsi_sempro_mk', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->string('kode_prodi', 20)->nullable()->index();
+            $table->integer('id_matakuliah')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('akd_skripsi_sempro_mk');
+    }
+}
